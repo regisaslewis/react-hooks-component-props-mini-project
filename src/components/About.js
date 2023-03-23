9b3d8ddcfe01730cbc39349bcc6d0d2a2c3e0d81
RegisLewis <regisaslewis@gmail.com>
2023-03-22 19:06:24 -0700

    retry

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from "react"
 function About({ about, image="https://via.placeholder.com/215" }) {
   return (
     <aside>
-      <img src={image} alt="blue logo" />
+      <img src={image} alt="blog logo" />
       <p>{about}</p>
     </aside>
   )
